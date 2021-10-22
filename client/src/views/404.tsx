8d7dbd3bd8d28406427dbff2cc12ebfc118da3bf
Nobu <nowobuwu@gmail.com>
2021-10-22 18:13:36 -0500

    rework boiler code

--- client/src/views/404.tsx
+++
@@ -1,9 +0,0 @@
-import React, { FC } from 'react'
-
-const  FourOFour: FC = () => {
-  return (
-    <h1>404</h1>
-  )
-}
-
-export default FourOFour
--- server/routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express'
-const router = Router()
-
-router.all("/", (req, res) => {
-  res.status(200).send("Hello World!")
-})
-
-export default router
